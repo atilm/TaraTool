14ec4c7d7209b9114e4fbd21ece8fc7c4c6af9cb
Andreas Thielmann <thielmann.andreas@googlemail.com>
2025-06-06 19:44:03 +0200

    generate initial file stubs

---
+++ domain/file_stubs.py
@@ -0,0 +1,35 @@
+class FileStub:
+    def __init__(self, file_path: str, file_content: str):
+        self.path = file_path
+        self.content = file_content
+
+
+file_stubs = [
+    FileStub("00_SystemDescritpion.md", 
+"""# System Description and Scope
+
+Describe the systems interfaces, data flows, and any external systems that interact with the system.
+Use drawio.png files or mermaid diagrams to illustrate the system architecture.
+"""),
+    FileStub("01_Assumptions.md", 
+"""# Assumptions
+
+| ID  | Name | Security Claim | Comment |
+| --- | ---- | -------------- | ------- |
+|     |      |                |         |
+"""),
+    FileStub("02_Assets.md", 
+"""# Assets
+
+| ID  | Name | Availability | Integrity | Confidentiality | Reasoning | Description |
+| --- | ---- | ------------ | --------- | --------------- | --------- | ----------- |
+|     |      |              |           |                 |           |             |
+"""),
+    FileStub("03_DamageScenarios.md", 
+"""# Damage Scenarios
+
+| ID  | Name | Safety | Operational | Financial | Privacy | Reasoning | Comment |
+| --- | ---- | ------ | ----------- | --------- | ------- | --------- | ------- |
+|     |      |        |             |           |         |           |         |
+"""),
+]
--- tara.py
+++ tara.py
@@ -1,23 +1,44 @@
-import sys
+import sys, os
+from domain.file_stubs import file_stubs
+
+usage_help = "Usage: python tara.py [init|check|gentrees|generate]"
 
 def init():
     print("Initializing...")
+    if os.path.exists("tara"):
+        print("Directory 'tara' already exists. Please remove it before initializing.")
+        sys.exit(1)
+    os.makedirs("tara")
+    
+    for file_stub in file_stubs:
+        with open(os.path.join("tara", file_stub.path), 'w') as f:
+            f.write(file_stub.content)
 
 def check():
     print("Checking...")
 
+def generate_attack_trees():
+    print("Generating attack trees...")
+
+def generate():
+    print("Generating...")
+
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python tara.py [init|check]")
+        print(usage_help)
         sys.exit(1)
     command = sys.argv[1]
     if command == "init":
         init()
     elif command == "check":
         check()
+    elif command == "gentrees":
+        generate_attack_trees()
+    elif command == "generate":
+        generate()
     else:
         print(f"Unknown command: {command}")
-        print("Usage: python tara.py [init|check]")
+        print(usage_help)
         sys.exit(1)
 
 if __name__ == "__main__":
